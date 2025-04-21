73694f6681f3fa4d646f0456db501f2a09fa8df0
kostabkpark <kostabkpark@gmail.com>
2025-04-21 12:27:23 +0900

    db add

--- index.js
+++ index.js
@@ -1,54 +1,119 @@
 import express from "express";
-import mysql from "mysql"
+//import negotiate from "express-negotiate";
+import mysql from "mysql";
+import dotenv from "dotenv";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 const host = "127.0.0.1";
 
-app.use(express.json())
-
-// DB_HOST=127.0.0.1
-// DB_PORT=3306
-// DB_USER=user_ex
-// DB_PASS=1234
+app.use(express.json());
+dotenv.config({path:'.env'});
 
 
-app.get('/boards', (req, res) => {
+const db = mysql.createConnection({
+  host : process.env.DB_HOST,
+  port : process.env.DB_PORT,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : "backend"
+})
+
+db.connect((err) => {
+  console.log("데이터베이스 연결 성공");
+  if(err) console.log(err);
+});
+
+// 콘텐츠 협상 관련 코드
+// app.get("/hello", (req, res) => {
+//   req.negotiate({
+//     "application/json;q=1.1": () => {
+//       res.send('{message : "hello world"');
+//     },
+//     html: () => {
+//       res.send("<h1>hello world</h1>");
+//     },
+//     xml: () => {
+//       res.send("<Message>hello<name>hongkildong</name></Message>");
+//     },
+//     default: () => {
+//       res.send("<h1>hello world</h1>");
+//     },
+//   });
+// });
+
+app.get("/api/boards", (req, res) => {
   console.log("글 목록 출력중");
-  res.send('글목록 출력')
-})
+  const sql = "select * from board_table";
+  db.query(sql, (err, results) => {
+      if(err) console.log(err);
+      else {
+        res.statusCode = 200;
+        res.setHeader('type', 'json');
+        res.send(results);
+      }
+  })
+});
 
-app.get('/boards/:id', (req, res) => {
+app.get("/api/boards/:id", (req, res) => {
   const id = req.params.id;
-  console.log("글 상세 조회", id);
-  res.send(`${id} 글 상세 조회`);
-})
+  const sql = "select * from board_table where id = ?";
+  db.query(sql, [id], (err, results) => {
+      if(err) console.log(err);
+      else {
+        if(results[0]) {
+          res.statusCode = 200;
+          res.setHeader('type', 'json');
+          res.send(results[0]);
+        } else {
+          let errorClass = {
+            status : 404, 
+            error : "Not Found",
+            message : "게시판자료를 찾을 수 없습니다."
+          }
+          res.statusCode = 404;
+          res.send(errorClass);
+        } 
+      }
+    });
+});
 
-app.post('/boards' , (req, res) => {
-  const {title, contents, writer, passwd} = req.body;
-  console.log("글 작성", req.body);
-  res.send(`${title} 글 작성중`);
-})
+// app.post("/api/boards", (req, res) => {
+//   const { title, contents, writer, passwd } = req.body;
+//   const sql = "insert into board_table (title, writer, password, contents) values (?,?,?,?)";
+//   db.query(sql, [title, writer, passwd, contents], (err, results) => {
+//     if(err) console.log(err);
+//     else { 
+//       const sql1 = "select * from board_table where id = ?";
+//       db.query(sql1, [results.insertId], (err1, results1) => {
+//         if(err1) console.log(err1);
+//         else {
+//           res.statusCode = 201;
+//           res.send(results1[0]);
+//         }
+//       });
+//     }
+// });
 
-app.put('/boards/:id' , (req, res) => {
+app.put("/api/boards/:id", (req, res) => {
   const id = req.params.id;
-  const {contents, passwd} = req.body;
+  const { contents, passwd } = req.body;
   console.log("글 수정 중", req.body);
   res.send(`${id} 글 수정중`);
-})
+});
 
-app.patch('/boards/:id' , (req, res) => {
+app.patch("/api/boards/:id", (req, res) => {
   const id = req.params.id;
   console.log("글 조회수 증가");
   res.send(`${id} 글 조회수 증가`);
-})
+});
 
-app.delete('/boards/:id' , (req, res) => {
+app.delete("/api/boards/:id", (req, res) => {
   const id = req.params.id;
   console.log("글 수정 중", id);
   res.send(`${id} 글 삭제중`);
-})
+});
 
 app.listen(port, host, () => {
-  console.log(`app listening on ${host}:${port}...`)
-})
+  console.log(`app listening on ${host}:${port}...`);
+});
